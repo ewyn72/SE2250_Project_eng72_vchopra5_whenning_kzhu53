137d05523b7159b2bb69fbe17ef80112aa97dd9c
Ethan Ng <ewyn101@gmail.com>
2019-02-23 15:40:36 -0500

    Basic ship setup
    Part 1 of phase 1

---
+++ Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    public float radius = 1f;
+    public float camWidth;
+    public float camHeight;
+
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+        if (pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+
+        transform.position = pos;
+    }
+}
