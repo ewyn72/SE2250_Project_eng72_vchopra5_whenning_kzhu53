fa8804ea5545eda3b5017c36968612cfdaa89b81
William Henning <whenning@uwo.ca>
2019-02-24 20:19:42 -0500

    Part 3 Enemies

---
+++ Assets/__Scripts/DeleteEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteEnemy : MonoBehaviour
+{
+    public float radius = 1f;
+    public float camWidth;
+    public float camHeight;
+
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > camWidth - radius)
+        {
+            Destroy(gameObject);
+        }
+        if (pos.x < -camWidth + radius)
+        {
+            Destroy(gameObject);
+        }
+        if (pos.y < -camHeight + radius)
+        {
+            Destroy(gameObject);
+        }
+        transform.position = pos;
+    }
+}
---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private float time = 0.0f;
+    public GameObject enemy0;
+    public GameObject enemy1;
+    public GameObject enemy2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 3.0f)
+        {
+            time = time - 3.0f;
+            GameObject enemy;
+            float enemyChoice = Random.Range(1, 4);
+            if (enemyChoice == 1)
+            {
+                enemy = Instantiate<GameObject>(enemy0);
+            }
+            else if (enemyChoice == 2)
+            {
+                enemy = Instantiate<GameObject>(enemy1);
+            }
+            else
+            {
+                enemy = Instantiate<GameObject>(enemy2);
+            }
+            float xPos = Random.Range(-50, 50);
+            enemy.transform.position = new Vector3(xPos, 50f);
+        }
+    }
+}
