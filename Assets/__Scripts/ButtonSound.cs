4a1a45db1cb06d8e22ca64f8e1b2d2d911a3f1a3
Ethan Ng <ewyn101@gmail.com>
2019-04-03 19:25:03 -0400

    Main Menu
    Added main menu and character selection.

---
+++ Assets/__Scripts/ButtonSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public AudioSource speaker;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+
+    public void HoverSound()
+    {
+        speaker.PlayOneShot(hoverFX); 
+    }
+    public void ClickSound()
+    {
+        speaker.PlayOneShot(clickFX);
+    }
+}
