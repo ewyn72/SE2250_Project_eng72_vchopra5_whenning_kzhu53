eb86dac44aa0fe9dd7573bb5e2ca9fef80768ecf
Ethan Ng <ewyn101@gmail.com>
2019-04-07 04:08:34 -0400

    Added Progress Bar

---
+++ Assets/__Scripts/ProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Slider slider;
+    public float maxTime; // In Seconds
+    private float currTime = 0;
+
+    private void Update()
+    {
+        slider.value = CalculateSliderValue();
+        if (currTime < maxTime)
+        {
+            currTime += Time.deltaTime;
+        }
+    }
+
+    float CalculateSliderValue()
+    {
+        return (currTime / maxTime);
+    }
+}
